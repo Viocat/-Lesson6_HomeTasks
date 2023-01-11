d98ef9464db5096fb4cdd9e8be14e78549138764
Viocat <44540969+Viocat@users.noreply.github.com>
2023-01-11 02:01:04 +0200

    Update dz18.js

--- task2/js/dz18.js
+++ task2/js/dz18.js
@@ -18,43 +18,80 @@
 
 // Вызов padString(‘hello’, 2) вернет ‘he’ — если число меньше, чем размер введенной строки, нужно строку обрезать при помощи метода substr
 
-let string=prompt('Wright something');
-let newLength=prompt('enter length of new string');
-let symbole=prompt('enter one symbole you`d like to use');
-let symboleLocation=Boolean(prompt('to locate symbol before text - type anything and press "ok"\n to leave default - press "ok" or "cansel"'));
 
-console.log(symboleLocation);
 
-let newString= padString(string, newLength, symbole, symboleLocation)
-alert(newString);
+function padString(string, newLength, symbole, symboleLocation = true) {
 
-function padString(string, newLength, symbole, symboleLocation){
-    let str=string;
-    let length=newLength;
-    let symbl=symbole;
-    let location=symboleLocation;
-    let result='';
+    if (typeof string !== 'string' || !string || string.trim().length < 1) return 'str error';
+    if (typeof newLength !== 'number') return 'new str length error';
+    if (!symbole || symbole.toString().trim().length > 1) return 'symbole error';
 
-    if(str===null||str.trim()===''){
-        result+='error string data\n';
-    } if(isNaN(length)||length===null||length.trim()===''){
-        result+='error length of new string data\n';
-    } if(symbl.length>1){
-        result+='error symbol data';
+    let str = string;
+    let symbl = symbole.toString();
+
+    if (newLength < str.length) return str.substring(0, length);
+    for (let i = 0, tmp = symbl; i < newLength - str.length; i++) {
+        symbl += tmp;
     }
+    if (symboleLocation === false) return symbl + str;
+    return str + symbl;
 
-    if(result!==''){
-        return result;
-    }else{
-        if(length<str.length){
-            result=str.substring(0,length);
-        }else{
-            for(let i=1, tmp=symbl;i<length-str.length;i++){
-                symbl+=tmp;
-            }
-            location===true?result=symbl+str:result=str+symbl;
-        }
-    }
-    console.log('finished result:' + result)
-    return result;
 }
+
+console.log(padString(' 12', 10, '#'));
+console.log(padString('abc', 5, 3, false));
+console.log(padString('abc', 10, '54*', false));
+console.log(padString('', 10, '54*', 1));
+console.log(padString('1', 10, '*', true));
+console.log(padString('lol', 4, '%', false));
+console.log(padString(null, 4, '%', false));
+console.log(padString('hm',null, '%', false));
+
+
+
+
+
+
+
+
+
+// let string=prompt('Wright something');
+// let newLength=prompt('enter length of new string');
+// let symbole=prompt('enter one symbole you`d like to use');
+// let symboleLocation=Boolean(prompt('to locate symbol before text - type anything and press "ok"\n to leave default - press "ok" or "cansel"'));
+
+// // console.log(symboleLocation);
+
+// let newString= padString(string, newLength, symbole, symboleLocation)
+// alert(newString);
+
+// function padString(string, newLength, symbole, symboleLocation){
+//     let str=string;
+//     let length=newLength;
+//     let symbl=symbole;
+//     let location=symboleLocation;
+//     let result='';
+
+//     if(str===null||str.trim()===''){
+//         result+='error string data\n';
+//     } if(isNaN(length)||length===null||length.trim()===''){
+//         result+='error length of new string data\n';
+//     } if(symbl.length>1){
+//         result+='error symbol data';
+//     }
+
+//     if(result!==''){
+//         return result;
+//     }else{
+//         if(length<str.length){
+//             result=str.substring(0,length);
+//         }else{
+//             for(let i=1, tmp=symbl;i<length-str.length;i++){
+//                 symbl+=tmp;
+//             }
+//             location===true?result=symbl+str:result=str+symbl;
+//         }
+//     }
+//     console.log('finished result:' + result)
+//     return result;
+// }
